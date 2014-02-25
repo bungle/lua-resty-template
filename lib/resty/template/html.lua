4ca8412ff31f5387e8196167098e383882157bf5
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-02-25 15:13:17 +0200

    Added HTML-helper (example for now).

---
+++ lib/resty/template/html.lua
@@ -0,0 +1,38 @@
+local template = require "resty.template"
+local setmetatable = setmetatable
+local pairs = pairs
+local concat = table.concat
+local escape = template.escape
+
+local html = {}
+
+function html.tag(content)
+    return setmetatable({}, {
+        __index = function(_, tag)
+            return function(_, attr)
+                return function()
+                    local r, a = {}, {}
+                    r[#r + 1] = "<"
+                    r[#r + 1] = tag
+                    if attr then
+                        for k, v in pairs(attr) do
+                            a[#a + 1] = k .. '="' .. escape(v) .. '"'
+                        end
+                        if #a > 0 then
+                            r[#r + 1] = " "
+                            r[#r + 1] = concat(a, " ")
+                        end
+                    end
+                    r[#r + 1] = ">"
+                    r[#r + 1] = escape(content)
+                    r[#r + 1] = "</"
+                    r[#r + 1] = tag
+                    r[#r + 1] = ">"
+                    return concat(r)
+                end
+            end
+        end
+    })
+end
+
+return html
