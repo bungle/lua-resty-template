ab4305da36a82cbf47821fbdc70461d7c3dbd4b3
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-02-26 03:14:15 +0200

    Moved CRC-32 to own file, now it works with normal Lua too (although no CRC-32 is done for normal Lua), at least yet.

--- lib/resty/template.lua
+++ lib/resty/template.lua
@@ -1,11 +1,4 @@
-local ffi = require "ffi"
-local bit = require "bit"
-local ffi_new = ffi.new
-local ffi_cast = ffi.cast
-local bxor = bit.bxor
-local band = bit.band
-local bnot = bit.bnot
-local rshift = bit.rshift
+local crc32 = require "resty.template.crc32"
 local setmetatable = setmetatable
 local tostring = tostring
 local assert = assert
@@ -17,23 +10,6 @@ local echo = print
 local type = type
 
 if ngx then echo = ngx.print end
-
--- CRC-32 Implemenatation
--- https://github.com/luapower/crc32/blob/master/crc32.lua
-local s_crc32 = ffi_new("const uint32_t[16]",
-    0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
-    0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
-    0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
-    0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c)
-
-local function crc32(buf)
-    local crc, sz, buf  = -1, #buf, ffi_cast("const uint8_t*", buf)
-    for i = 0, sz - 1 do
-        crc = bxor(rshift(crc, 4), s_crc32[bxor(band(crc, 0xF), band(buf[i], 0xF))])
-        crc = bxor(rshift(crc, 4), s_crc32[bxor(band(crc, 0xF), rshift(buf[i], 4))])
-    end
-    return bnot(crc)
-end
 
 local VIEW_ACTIONS = {
     ["{%"] = function(code) return code end,
