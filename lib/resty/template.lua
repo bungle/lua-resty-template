3beac01023dbbc3558a2f9606b36fb4cd15a4e65
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-12-29 12:57:05 +0200

    Removed unneeded parenthesis.

--- lib/resty/template.lua
+++ lib/resty/template.lua
@@ -44,7 +44,7 @@ end
 local function read_file(path)
     local file = open(path, "rb")
     if not file then return nil end
-    local content = file:read("*a")
+    local content = file:read "*a"
     file:close()
     return content
 end
