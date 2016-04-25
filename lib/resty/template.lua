7a95ca6a3c42c88eabc21021b9bf582963b503e7
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2016-04-25 12:05:41 +0300

    Release 1.6.

--- lib/resty/template.lua
+++ lib/resty/template.lua
@@ -56,7 +56,7 @@ if not ok then newtab = function() return {} end end
 local caching = true
 local template = newtab(0, 12)
 
-template._VERSION = "1.6-dev"
+template._VERSION = "1.6"
 template.cache    = {}
 
 local function enabled(val)
